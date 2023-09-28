88041ebdba20f6b46889511f62f7af06460a30b5
Hans Ole Hatzel <hhatzel@gmail.com>
2023-09-28 07:37:52 +0200

    Enable automatic translation

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from wikidata.client import Client
 import json
 from pathlib import Path
+import random
 from collections import defaultdict
+import re
+import time
 import os
 from lxml import etree
 import time
@@ -11,6 +14,13 @@ import wikipediaapi
 from tqdm import tqdm
 from matplotlib import pyplot as plt
 from urllib.error import HTTPError
+import sklearn
+import sentence_transformers
+import ersatz
+import torch
+from itertools import groupby
+
+import dataset
 
 app = Typer()
 
@@ -45,6 +55,7 @@ def scrape_wikidata():
         i += 1
         out_file = open(file_name, "w")
         try:
+            entity = None
             entity = c.get(wid, load=True)
         except HTTPError as e:
             if e.code == 404:
@@ -53,21 +64,27 @@ def scrape_wikidata():
             else:
                 print("HTTP ERROR:", e)
                 break
-        json.dump(entity.data, out_file)
-        out_file.close()
+        finally:
+            if entity is not None:
+                json.dump(entity.data, out_file)
+            else:
+                json.dump({}, out_file)
+            out_file.close()
         time.sleep(0.1)
 
 
 def build_clients():
-    return {lang: wikipediaapi.Wikipedia(lang.removesuffix("wiki"), extract_format=wikipediaapi.ExtractFormat.HTML) for lang in LANGS}
+    return {lang: wikipediaapi.Wikipedia(language=lang.removesuffix("wiki"), extract_format=wikipediaapi.ExtractFormat.HTML, user_agent='StorySummaryBot (hans.ole.hatzel@uni-hamburg.de)') for lang in LANGS}
 
 @app.command()
-def scrape_wikipedia(target_langs: list[str] = LANGS[2:]):
+def scrape_wikipedia(target_langs: list[str] = LANGS):
     # Sometimes summary is in an h2 e.g. english version of this: https://www.wikidata.org/wiki/Q1194637
     clients = build_clients()
     for work_file in tqdm(glob.glob("data/wikidata/*/*.json")):
         data = json.load(open(work_file))
-        if len(set(data["sitelinks"].keys()) & {"dewiki", "enwiki"}) == 2:
+        if len(data) == 0:
+            continue
+        if len(set(data.get("sitelinks", {}).keys()) & {"dewiki", "enwiki"}) == 2:
             path = f"data/wikipedia/{data['title'][1:3]}/{data['title'][1:]}"
             os.makedirs(path, exist_ok=True)
             for lang in target_langs:
@@ -76,6 +93,7 @@ def scrape_wikipedia(target_langs: list[str] = LANGS[2:]):
                     if title is not None:
                         page = clients[lang].page(title)
                         json.dump({s.title : s.full_text() for s in page.sections}, open(path + f"/{lang}.json", "w"))
+                        time.sleep(0.1)
 
 
 def extract_summary(sections, lang):
@@ -88,26 +106,29 @@ def extract_summary(sections, lang):
 def html_to_plain(markup, parser=etree.HTMLParser(recover=True)):
     markup = "<root>" + markup + "</root>"
     root = etree.fromstring(markup, parser=parser)
-    elements = root.xpath(".//root/*[self::h3 or self::h2 or self::p]")
+    elements = root.xpath(".//root/*[self::h3 or self::h2 or self::p or self::dd or self::dl]")
     out_text = []
     for el in elements:
-        if el.tag == "p":
-            out_text.append("".join(el.itertext()) or "")
+        if el.tag in ["p", "dl", "dd"]:
+            out_text.append("".join(el.itertext()).strip() + "\n")
         elif el.tag == "h2":
-            out_text.append(("# " + el.text) or "")
+            out_text.append(("# " + el.text.strip() + "\n") if el.text is not None else "")
         elif el.tag == "h3":
-            out_text.append(("## " + el.text) or "")
+            out_text.append(("## " + el.text.strip() + "\n") if el.text is not None else "")
         elif el.tag == "h1":
             print("H1", el.text or "")
         else:
             print("Unknown tag")
-    return " ".join(out_text)
+    return "".join(out_text).strip()
 
 
 @app.command()
 def extract_summaries():
     for work_dir in tqdm(glob.glob("data/wikipedia/*/*")):
-        wikidata_info = json.load(open(work_dir.replace("/wikipedia/", "/wikidata/") + ".json"))
+        try:
+            wikidata_info = json.load(open(work_dir.replace("/wikipedia/", "/wikidata/") + ".json"))
+        except FileNotFoundError:
+            continue
         wikidata_id = wikidata_info["title"]
         summary_dict = {}
         summary_section_dict = {}
@@ -115,17 +136,19 @@ def extract_summaries():
             path = Path(work_dir) / file_name
             data = json.load(open(path))
             lang = path.name.removesuffix("wiki.json")
-            summary_dict[lang], summary_section_dict[lang] = extract_summary(data, lang)
-            if lang in ["es", "fr", "it"] and summary_dict[lang] is None:
-                print("Url", wikidata_info["sitelinks"][lang + "wiki"]["url"])
-                print("Sections", data.keys())
-                breakpoint()
+            summary_pair = extract_summary(data, lang)
+            if summary_pair is not None:
+                summary_dict[lang], summary_section_dict[lang] = summary_pair
+                if lang in ["es", "fr", "it"] and summary_dict[lang] is None:
+                    print("Url", wikidata_info["sitelinks"][lang + "wiki"]["url"])
+                    print("Sections", data.keys())
         if  len([text for text in summary_dict.values() if text is not None]) >= 2:
             labels = wikidata_info.get("labels", {})
             title_en = labels.get("en", {}).get("value")
             title_de = labels.get("de", {}).get("value")
             description_en = wikidata_info.get("descriptions", {}).get("en", {}).get("value")
             title = title_en or labels.get(list(labels.keys())[0])
+            plain_text_summaries = {k: html_to_plain(v) for k, v in summary_section_dict.items() if v is not None}
             out = {
                     "wikidata_id": wikidata_id,
                     "title_en": title_en,
@@ -133,7 +156,7 @@ def extract_summaries():
                     "titles": {k : labels.get(k) for k in summary_dict.keys()},
                     "title": title,
                     "description": description_en,
-                    "summaries": {k: html_to_plain(v) for k, v in summary_dict.items() if v is not None},
+                    "summaries": plain_text_summaries,
                     "summary_sections": summary_section_dict,
             }
             out_file_path = f"data/summaries/{wikidata_id[1:3]}/{wikidata_id[1:]}.json"
@@ -149,9 +172,9 @@ def get_all_summaries(limit=None):
     for file_name in glob.glob("data/summaries/*/*.json"):
         i += 1
         data = json.load(open(file_name))
-        if data["summaries"]["de"] == "":
+        if data["summaries"].get("de", "") == "":
             continue
-        if data["summaries"]["en"] == "":
+        if data["summaries"].get("en", "") == "":
             continue
         summaries["en"].append(data["summaries"]["en"])
         summaries["de"].append(data["summaries"]["de"])
@@ -173,13 +196,103 @@ def analyze_similarities():
         print(ids[m])
         # f1s[m]
     # max_pos = f1s.argmax()
-    plt.hist(f1s, bins=40)
-    plt.show()
+    plt.hist(recalls, bins=41)
+    plt.savefig("sims.pdf")
+
+@app.command()
+def test_annotated(annotated_tsv: str):
+    import torch.nn.functional as F
+    label_dict = {}
+    for line in open(annotated_tsv):
+        id_, label = line.strip().split("\t")
+        label_dict[id_[1:]] = label == "True"
+    ds = dataset.SummaryDataset("data", only_include=label_dict.keys())
+    dev, test = ds.stratified_split(label_dict)
+    data = dev
+    model = sentence_transformers.SentenceTransformer("sentence-transformers/sentence-t5-base")
+    embeddings_de = model.encode([pair[1].summaries["de"] for pair in data], show_progress_bar=True, convert_to_tensor=True)
+    embeddings_en = model.encode([pair[1].summaries["en"] for pair in data], show_progress_bar=True, convert_to_tensor=True)
+    sims = F.cosine_similarity(embeddings_de, embeddings_en)
+    label_tensor = torch.tensor([pair[0] for pair in data])
+    def get_metrics(label_tensor, sims, decision_boundary):
+        precision = label_tensor[sims < decision_boundary].sum() / len(label_tensor[sims < decision_boundary])
+        recall = label_tensor[sims < decision_boundary].sum() / label_tensor.sum()
+        f1 = 2 * (precision * recall / (precision + recall))
+        return precision, recall, f1
+    for decision_boundary in [0.8, 0.9, 0.91, 0.92, 0.93, 0.933, 0.934, 0.935, 0.936, 0.937, 0.94, 0.95]:
+        print("=====Decision Boundary", decision_boundary)
+        precision, recall, f1 = get_metrics(label_tensor, sims, decision_boundary)
+        print("Recall", recall)
+        print("Precision", precision)
+        print("F1", f1)
+    print(sims)
+
+
+@app.command()
+def split_docs():
+    line_ids = defaultdict(list)
+    os.makedirs("data/to_translate/", exist_ok=True)
+    out_files = {lang: open(f"data/to_translate/{lang}.txt", "w") for lang in SUMMARY_SECTIONS.keys()}
+    out_line_ids = {lang: open(f"data/to_translate/{lang}.tsv", "w") for lang in SUMMARY_SECTIONS.keys()}
+    for file_name in tqdm(glob.glob("data/summaries/*/*.json"), desc="Splitting work summaries"):
+        data = json.load(open(file_name))
+        for lang, summary in data["summaries"].items():
+            paragraphs = []
+            wiki_id = os.path.splitext(os.path.basename(file_name))[0]
+            for i, p in enumerate(summary.strip().split("\n")):
+                if len(p.strip()) == 0:
+                    continue
+                split = ersatz.split_text(model=lang.replace("it", "default-multilingual"), text=p)
+                out_files[lang].writelines([s + "\n" for s in split])
+                out_line_ids[lang].writelines([f"{wiki_id}_{i}\n" for _ in range(len(split))])
+
+
+@app.command()
+def add_translations(lang, translated_file: str, line_mapping_file: str):
+    line_iterator = zip([info.strip().split("_") for info in open(line_mapping_file)], open(translated_file))
+    grouped_by_wiki_id = groupby(line_iterator, key=lambda line: line[0][0])
+    translations = {}
+    for wiki_id, paragraphs in grouped_by_wiki_id:
+        grouped_by_paragraph = groupby(paragraphs, key=lambda line: line[0][1])
+        text = "\n".join([" ".join([line[1].strip() for line in lines]) for k, lines in grouped_by_paragraph])
+        translations[wiki_id] = text
+    for wiki_id, text in translations.items():
+        basename = f"data/summaries/{wiki_id[:2]}/{wiki_id}"
+        data_file_path = basename + ".json"
+        temp_file_path = basename + ".temp"
+        data = json.load(open(data_file_path))
+        data["en_translated_summaries"] = data.get("en_translated_summaries", [])
+        with open(temp_file_path, "w") as temp_file:
+            data["en_translated_summaries"].append({"text": text, "translation_tool": "nllb-200-3.3B", "source_lang": lang})
+            json.dump(data, temp_file)
+        os.replace(temp_file_path, data_file_path)
+
+
+@app.command()
+def build_subset(out_path: str = "pairs.csv", in_path: str = "pairs_annotated.txt"):
+    PAIRS = re.compile(r"=*Summary Pair\n=========de===========\n(.*?)=========en===========\n(.*?)\n\n([A-Z\?]{1,10}\n)?Is translation\? (y|n)$", re.MULTILINE | re.DOTALL)
+    #PAIRS = re.compile(r"^=*Summary Pair\n=========de===========\n(.*?)=========en===========\n(.*?)\n\n[A-Z\n]*?Is translation? (y|n)$", re.MULTILINE | re.DOTALL)
+    text = "".join(open(in_path).readlines())
+    summaries, ids = get_all_summaries()
+    out_file = open(out_path, "w")
+    for match in PAIRS.findall(text):
+        de, en, _, label = match
+        label = label == "y"
+        wiki_id = summaries["de"].index(de)
+        out_file.write("\t".join([ids[wiki_id], str(label)]) + "\n")
+    #subset = random.choices(list(zip(summaries["de"], summaries["en"])), k=100)
+    #out_file = open(out_path, "w")
+    #for summary_de, summary_en in subset:
+    #    out_file.write("=======================================Summary Pair\n")
+    #    out_file.write("=========de===========\n")
+    #    out_file.write(summary_de)
+    #    out_file.write("=========en===========\n")
+    #    out_file.write(summary_en)
+    #    out_file.write("\n\nIs translation? yn\n")
 
 
 @app.command()
 def sbert():
-    import sentence_transformers
     summaries, ids = get_all_summaries(limit=1200)
     model = sentence_transformers.SentenceTransformer('all-MiniLM-L6-v2')
     de = model.encode(summaries["de"])
